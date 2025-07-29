362513296621ca7d1d634fbd4cc6302078dde956
Andrew Slack <me@andyslack.com>
2025-07-29 09:35:42 +0200

    Fix lint and prettier

--- src/sandbox/sandbox.controller.ts
+++ src/sandbox/sandbox.controller.ts
@@ -1,12 +1,12 @@
 import { SandboxService } from './sandbox.service'
-import { Controller, Get, Req } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 
 @Controller('/app/aweber/sandbox')
 export class SandboxController {
 	constructor(private readonly sandboxService: SandboxService) {}
 
 	@Get()
-	async sandbox(@Req() req: any): Promise<{ url: string }> {
-		return await this.sandboxService.run(req)
+	async sandbox(): Promise<{ url: string }> {
+		return await this.sandboxService.run()
 	}
 }
